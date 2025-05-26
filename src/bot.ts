c7942f676ea50db7a0f6b211b14bbad3454fab60
PromiSe#### <vicimpa@ya.ru>
2025-05-26 21:58:47 +0300

    Fix bot logic

--- src/bot.ts
+++ src/bot.ts
@@ -5,7 +5,7 @@ import env from "./env";
 export const bot = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildVoiceStates
   ]
 });
 
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,40 @@
-import { VoiceChannel, VoiceState } from "discord.js";
+import { GuildMember, VoiceChannel, VoiceState, type VoiceBasedChannel } from "discord.js";
 
 import { bot } from "./bot";
 import { config } from "./config";
 
-const ignore = new WeakSet<VoiceState>();
+const IGNORE_CACHE = new WeakMap<VoiceChannel | VoiceBasedChannel, boolean>();
+const IGNORED = new WeakSet<VoiceState>();
+
+function isIgnore(channel: VoiceChannel | VoiceBasedChannel) {
+  var output = false;
+  return IGNORE_CACHE.get(channel) ?? (
+    IGNORE_CACHE.set(channel, output = (
+      config.ignore.channels.includes(channel.id) && (
+        !channel.parentId || config.ignore.category.includes(channel.parentId)
+      )
+    )),
+    output
+  );
+}
+
+function isCanSpeak(channel: VoiceChannel | VoiceBasedChannel, member: GuildMember) {
+  return isIgnore(channel) || channel.permissionsFor(member).has('Speak');
+}
 
 async function loop() {
   for (const [, guild] of bot.guilds.cache) {
     for (const [, channel] of guild.channels.cache) {
       if (!(channel instanceof VoiceChannel)) continue;
-      const isIgnore = config.ignore.channels.includes(channel.id) && (
-        !channel.parentId || config.ignore.category.includes(channel.parentId)
-      );
+
       for (const [, member] of channel.members) {
-        const canSpeak = isIgnore || channel.permissionsFor(member).has('Speak');
-        if (!member.voice || member.voice.serverMute === !canSpeak) continue;
-        if (ignore.has(member.voice)) continue;
+        const canSpeak = isCanSpeak(channel, member);
+        if (!member.voice) continue;
+        if (member.voice.serverMute === !canSpeak) {
+          IGNORED.delete(member.voice);
+          continue;
+        }
+        if (IGNORED.has(member.voice)) continue;
         await member.voice.setMute(!canSpeak).catch(console.error);
       }
     }
@@ -23,15 +42,24 @@ async function loop() {
 }
 
 bot.on('voiceStateUpdate', (old, state) => {
-  ignore.delete(old); ignore.delete(state);
-  if (!state.member || !state.channel) return;
-  const canSpeak = state.channel.permissionsFor(state.member).has('Speak');
-  if (old.channel?.id === state.channel.id && state.serverMute === canSpeak) {
-    ignore.add(state);
+  IGNORED.delete(old);
+  IGNORED.delete(state);
+
+  if (!old.channel || !state.channel) return;
+  if (!old.member || !state.member) return;
+  if (old.channel !== state.channel) return;
+  if (typeof old.serverMute !== 'boolean' || typeof state.serverMute !== 'boolean') return;
+  const canSpeak = isCanSpeak(state.channel, state.member);
+
+  if (old.serverMute !== state.serverMute) {
+    if (state.serverMute !== !canSpeak) {
+      IGNORED.add(state);
+    }
   }
+
 });
 
 while (true) {
   await loop();
-  await new Promise(resolve => setTimeout(resolve, 100));
+  await new Promise(resolve => setTimeout(resolve, 50));
 }
